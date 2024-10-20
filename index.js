bc8544561e64801a5e48167a369e5433c772efca
VinaySavla <savlavinay77@gmail.com>
2024-10-20 16:08:08 +0530

    uncommented one to may foreign keys

--- index.js
+++ index.js
@@ -36,17 +36,17 @@ const db = require("./models");
 //Foreign Keys
 
 // //One to Many
-// db.Subjects.hasMany(db.Chapters, { foreignKey: 'SubjectID', as: 'chapters' });
-// db.Chapters.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
+db.Subjects.hasMany(db.Chapters, { foreignKey: 'SubjectID', as: 'chapters' });
+db.Chapters.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
 
-// db.Subjects.hasMany(db.Notes, { foreignKey: 'SubjectID', as: 'notes' });
-// db.Notes.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
+db.Subjects.hasMany(db.Notes, { foreignKey: 'SubjectID', as: 'notes' });
+db.Notes.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
 
-// db.Subjects.hasMany(db.Questions, { foreignKey: 'SubjectID', as: 'questions' });
-// db.Questions.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
+db.Subjects.hasMany(db.Questions, { foreignKey: 'SubjectID', as: 'questions' });
+db.Questions.belongsTo(db.Subjects, { foreignKey: 'SubjectID', as: 'subjects' });
 
-// db.Chapters.hasMany(db.Questions, { foreignKey: 'ChapterID', as: 'questions' });
-// db.Questions.belongsTo(db.Chapters, { foreignKey: 'ChapterID', as: 'chapters' });
+db.Chapters.hasMany(db.Questions, { foreignKey: 'ChapterID', as: 'questions' });
+db.Questions.belongsTo(db.Chapters, { foreignKey: 'ChapterID', as: 'chapters' });
 
 
 // //Many to Many
